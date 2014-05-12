45e476c532aa93ea02df280dec3abbcd049a9b0b
Meka <antoine.hoarau@inria.fr>
2014-05-12 17:22:56 +0200

    removed rt service verbose

--- src/m3rt/rt_system/rt_system.cpp
+++ src/m3rt/rt_system/rt_system.cpp
@@ -132,11 +132,11 @@ void * rt_system_thread(void * arg)
 		end = nano2count(rt_get_cpu_time_ns());
 		dt=end-start;
 
-		if (tmp_cnt++ == 1000)
+		/*if (tmp_cnt++ == 1000)
 		{
 		  M3_INFO("Loop computation time : %d us\n",static_cast<int>((count2nano(dt)/1000)));
 		  tmp_cnt = 0;
-		}
+		}*/
 		/*
 		Check the time it takes to run components, and if it takes longer
 		than our period, make us run slower. Otherwise this task locks
